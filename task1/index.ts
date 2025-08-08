0d35008e99940887e8173eb7ef98a81c6c8f34bb
mrojek-gr <mrojek@griddynamics.com>
2025-08-08 17:52:24 +0200

    task1 tests

--- task1/index.ts
+++ task1/index.ts
@@ -1,4 +1,4 @@
-import { fetchIsUserNameAvailable } from 'tasks/task1/fetchIsUserNameValid';
+import { fetchIsUserNameAvailable } from "tasks/task1/fetchIsUserNameValid";
 
 export const validateUserName = async (userName: string): Promise<boolean> => {
   if (!/^[a-zA-Z][0-9a-zA-Z]{2,}$/.test(userName)) {
--- task1/task1.test.ts
+++ task1/task1.test.ts
@@ -1,14 +1,61 @@
-import { validateUserName } from 'tasks/task1/index';
-import { fetchIsUserNameAvailable } from 'tasks/task1/fetchIsUserNameValid';
+import { validateUserName } from "tasks/task1/index";
+import { fetchIsUserNameAvailable } from "tasks/task1/fetchIsUserNameValid";
 
-jest.mock('task2/fetchIsUserNameValid', 
-  // write your mock here
-);
+jest.mock("tasks/task1/fetchIsUserNameValid", () => ({
+  fetchIsUserNameAvailable: jest.fn(),
+}));
 
-describe('task2', () => {
-  it('returns false if name has length less than 3 symbols', () => {
-    // write your first unit test here
+const mockedFetch = jest.mocked(fetchIsUserNameAvailable);
+
+describe("userNameValidation", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  // ...and add more test cases
+  it("returns false if name has length less than 3 symbols", async () => {
+    const result = await validateUserName("Ma");
+    expect(result).toBe(false);
+    expect(mockedFetch).not.toHaveBeenCalled();
+  });
+
+  it("returns false if name has not only alphanumeric symbols", async () => {
+    const result = await validateUserName("Mateusz!");
+    expect(result).toBe(false);
+    expect(mockedFetch).not.toHaveBeenCalled();
+  });
+
+  it("returns false if name contains spaces", async () => {
+    const result = await validateUserName("Ma teusz");
+    expect(result).toBe(false);
+    expect(mockedFetch).not.toHaveBeenCalled();
+  });
+
+  it("returns false if name starts with number", async () => {
+    const result = await validateUserName("1Mateusz");
+    expect(result).toBe(false);
+    expect(mockedFetch).not.toHaveBeenCalled();
+  });
+
+  it("returns false if name is not unique", async () => {
+    mockedFetch.mockResolvedValueOnce(false);
+    const result = await validateUserName("Mateusz");
+    expect(mockedFetch).toHaveBeenCalledWith("Mateusz");
+    expect(result).toBe(false);
+  });
+
+  it("returns true if name is unique", async () => {
+    mockedFetch.mockResolvedValueOnce(true);
+    const result = await validateUserName("Mateusz");
+    expect(mockedFetch).toHaveBeenCalledWith("Mateusz");
+    expect(result).toBe(true);
+  });
+
+  it("returns false if fetchIsUserNameAvailable throws exception", async () => {
+    mockedFetch.mockImplementationOnce(() => {
+      throw new Error("Sync failure");
+    });
+    const result = await validateUserName("Mateusz");
+    expect(mockedFetch).toHaveBeenCalledWith("Mateusz");
+    expect(result).toBe(false);
+  });
 });
